1cb623cba930adb900612a7173a1b918e0657be6
Tony Lattke <tonylattke@gmail.com>
2015-02-01 17:15:54 +0100

    tuples

---
+++ 2_data_structures.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+####################################### Tuples ##########################################
+
+# Tuple
+mytuple=("Bruce","Wayne")
+echo $mytuple
+
+# Triplet
+mytriplet=("Red","Green","Blue")
+echo $mytriplet
+
+# N-Tuple
+ntuple=("Batman","Superman","Wonder Woman","Green Light","Flash","J'onn")
+echo $ntuple
+
+# Access (to do)
